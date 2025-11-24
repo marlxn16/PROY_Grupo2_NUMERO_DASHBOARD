97be55322dcb280122d07b5a8ac0376ed59bda93
Christopher Armas <christopherarmas4@gmail.com>
2025-11-24 17:30:59 +0000

    Empezamos con el DOM base, variables y el addeventlistener de los eventos

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+//DOM
+// Creamos las constantes de los botones y a los contenedores
+const btn_agregar = document.getElementById('btn_agregar');
+const btn_reporte = document.getElementById('btn_reporte');
+const txt_tabla = document.getElementById('txt_tabla');
+const txt_alerta = document.getElementById('txt_alerta');
+const txt_reporte = document.getElementById('txt_reporte');
+
+let estudiantes = [];
+//el evento click para el boton agregar 
+btn_agregar.addEventListener('click', function(event){
+    
+//Capturamos los datos
+    let nombre = document.getElementById('txt_nombre').value;
+    let edad = document.getElementById('txt_edad').value;
+    let nota = parseFloat(document.getElementById('txt_nota').value);
+//limpiamos los mesajes anteriores
+    txt_alerta.innerHTML = '';
+    txt_reporte.innerHTML = '';
+//Validaciones
+
+
+
+
+//Ciclo WHILE
+
+
+
+
+//Ciclo FOR
+
+
+
+//Ciclo DO...WHILE
+
+
+
+
+});
